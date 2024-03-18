af105fe0f32e9c260ccdfab5b6b0a17e3579773d
mleal <mleal@indexar.cl>
2024-03-18 00:31:13 -0300

    conexión bd mysql

--- src/main/java/com/matias/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/matias/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,13 +1,53 @@
 package com.matias.curso.springboot.jpa.springbootjpa;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matias.curso.springboot.jpa.springbootjpa.entities.Person;
+import com.matias.curso.springboot.jpa.springbootjpa.repositories.PersonRepository;
+
 @SpringBootApplication
-public class SpringbootJpaApplication {
+public class SpringbootJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private PersonRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		findOne();
+	}
+
+	public void findOne() {
+		// Person person = null;
+		// Optional<Person> optionalPerson = repository.findById(9L);
+		// if (optionalPerson.isPresent()) {
+		// 	person = optionalPerson.get();
+		// }
+		// System.out.println(person);
+		repository.findById(1L).ifPresent(person -> System.out.println(person));
+	}
+
+	public void list() {
+		// List<Person> persons = (List<Person>) repository.findAll();
+		// List<Person> persons = (List<Person>) repository.buscarByProgrammingLanguage("Java", "Andres");
+
+		List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Java", "Andres");
+		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> personsValues = repository.obtenerPersonDataByProgrammingLanguage("Java");
+		personsValues.stream().forEach(person ->{
+			System.out.println(person[0] + " es experto en " + person[1]);
+		});
 	}
 
 }
---
+++ src/main/java/com/matias/curso/springboot/jpa/springbootjpa/repositories/PersonRepository.java
@@ -0,0 +1,30 @@
+package com.matias.curso.springboot.jpa.springbootjpa.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.matias.curso.springboot.jpa.springbootjpa.entities.Person;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.name=?1")
+    List<Object[]> obtenerPersonData(String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Object[]> obtenerPersonData(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.programmingLanguage=?1")
+    List<Object[]> obtenerPersonDataByProgrammingLanguage(String programmingLanguage);
+}
