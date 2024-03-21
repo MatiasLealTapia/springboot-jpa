5bb8ac9f9ca05aa75755d7a3ea9c5a1145561121
mleal <mleal@indexar.cl>
2024-03-21 19:54:01 -0300

    Delete 2, select unica propiedad

--- src/main/java/com/matias/curso/springboot/jpa/springbootjpa/repositories/PersonRepository.java
+++ src/main/java/com/matias/curso/springboot/jpa/springbootjpa/repositories/PersonRepository.java
@@ -10,6 +10,9 @@ import com.matias.curso.springboot.jpa.springbootjpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+    @Query("select p.name from Person p where p.id=?1")
+    String getNameById(Long id);
+
     @Query("select p from Person p where p.id=?1")
     Optional<Person> findOne(Long id);
 
