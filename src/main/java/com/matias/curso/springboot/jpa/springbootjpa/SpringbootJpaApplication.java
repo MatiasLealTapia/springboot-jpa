21b7668d257eb2c4c90e828c0e6443edef552fca
mleal <mleal@indexar.cl>
2024-03-21 23:58:06 -0300

    select con concat

--- src/main/java/com/matias/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/matias/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -26,7 +26,27 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		delete2();
+		personalizedQueries();
+	}
+
+	@Transactional(readOnly = true)
+	public void personalizedQueries() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("=========== Consulta solo el nombre por el Id ===========");
+		System.out.println("Ingrese el ID para leer nombre de persona:");
+		Long id = scanner.nextLong();
+		scanner.close();
+
+		String name = repository.getNameById(id);
+		System.out.println(name);
+
+		Long idDb = repository.getIdById(id);
+		System.out.println(idDb);
+
+		String fullName = repository.getFullNameById(id);
+		System.out.println(fullName);
 	}
 
 	@Transactional
